e1a35001078997bf2714db514fb020f8fc055cf1
KangJun <rkdwnsdud37@gmail.com>
2026-04-23 00:59:12 +0900

    Health System Apply

---
+++ ArcadeGame/Assets/Script/System/HealthSystem.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour, IDamageable
+{
+    public float maxHealth = 100f;
+    private float _currentHealth;
+
+    public Action<float, float> onHealthChange;
+    public Action onDied;
+
+    private void OnEnable()
+    {
+        _currentHealth = maxHealth;
+        onHealthChange?.Invoke(_currentHealth, maxHealth);
+    }
+
+    public void TakeDamage(DamageInfo damageInfo)
+    {
+        if (_currentHealth <= 0) return;
+        _currentHealth -= damageInfo.damage;
+        onHealthChange?.Invoke(_currentHealth, maxHealth);
+        if (_currentHealth <= 0)
+        {
+            _currentHealth = 0;
+            onDied?.Invoke();
+        }
+    }
+}
