9cbe03b3c9950ec1f50fa8757c684dab90559d2c
KangJun <rkdwnsdud37@gmail.com>
2026-04-26 21:17:22 +0900

    Bullet Attack method Modify

--- ArcadeGame/Assets/Script/Item/PoolAbleItem/Bullet.cs
+++ ArcadeGame/Assets/Script/Item/PoolAbleItem/Bullet.cs
@@ -8,20 +8,23 @@ public class Bullet : PoolAble , IUpdateable
 {
     [Header("Bullet Settings")]
     public float speed = 30.0f;
+    public LayerMask hitMask;
+    public BulletData bulletData;
     
     private TrailRenderer _trail;
     private MeshRenderer _meshRenderer;
-    private Collider _collider;
+    private Vector3 _previousPosition;
     private bool _isDead = false;
     private void Awake()
     {
         _trail = GetComponent<TrailRenderer>();
         _meshRenderer = GetComponentInChildren<MeshRenderer>();
-        _collider = GetComponent<Collider>();
+
     }
 
     private void OnEnable()
     {
+        _previousPosition = transform.position;
         GameManager.Instance.AddBullet(this);
     }
     private void OnDisable()
@@ -32,13 +35,16 @@ public class Bullet : PoolAble , IUpdateable
     
     public void OnTick(float deltaTime)
     {
-        BulletMove(deltaTime);
+        if (_isDead) return;
+        HitCheck(deltaTime);
+        if(!_isDead)
+            BulletMove(deltaTime);
     }
 
     private void BulletMove(float deltaTime)
     {
-        if (_isDead) return;
         transform.position += transform.forward * (speed * deltaTime);
+        _previousPosition = transform.position;
     }
 
     public void Init()
@@ -46,18 +52,52 @@ public class Bullet : PoolAble , IUpdateable
         _trail.emitting = true;
         _trail.Clear();
         _isDead = false;
-        _collider.enabled = true;
         _meshRenderer.enabled = true;
     }
-    private void OnTriggerEnter(Collider other)
+
+    private void HitCheck(float deltaTime)
     {
-        _isDead = true;
-        _collider.enabled = false;
-        _trail.emitting = false;
-        _meshRenderer.enabled = false;
-        StartCoroutine(DelayDisable(_trail.time));
+        float distanceThisFrame = speed * deltaTime;
+        if (Physics.Raycast(_previousPosition, transform.forward, out RaycastHit hit, distanceThisFrame, hitMask))
+        {
+            if (hit.collider.TryGetComponent(out HealthSystem healthSystem))
+            {
+                if (healthSystem.isInvincible)
+                    return;
+                DamageInfo damageInfo = new DamageInfo
+                {
+                    damage = bulletData.damage
+                };
+                healthSystem.TakeDamage(damageInfo);
+            }
+            Spawn_VFX(hit);
+            _isDead = true;
+            _trail.emitting = false;
+            _meshRenderer.enabled = false;
+            transform.position = hit.point;
+            
+            StartCoroutine(DelayDisable(_trail.time));
+        }
     }
 
+    private void Spawn_VFX(RaycastHit hit)
+    {
+        if (hit.collider.TryGetComponent(out SurfaceProp hitSurface))
+        {
+            var vfxName = hitSurface.surfaceType switch
+            {
+                SurfaceType.Metal => "MetalVFX",
+                SurfaceType.Flesh => "BloodVFX",
+                SurfaceType.Wood => "WoodVFX",
+                SurfaceType.Dirt => "DirtVFX",
+                _ => "DefaultVFX"
+            };
+            var hitVFX = ObjectPoolManager.instance.GetGo(vfxName);
+            if (!hitVFX) return;
+            hitVFX.transform.position = hit.point;
+            hitVFX.transform.rotation = Quaternion.LookRotation(hit.normal);
+        }
+    }
     IEnumerator DelayDisable(float delay)
     {
         yield return new WaitForSeconds(delay);
